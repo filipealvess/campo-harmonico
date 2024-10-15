e60825675642e439c9d93502bc94434b0aeb312f
Filipe Alves <filipe.alvesdsouza@gmail.com>
2024-10-15 16:36:16 -0300

    Clean files

---
+++ src/app/_layout.tsx
@@ -0,0 +1,31 @@
+import { useFonts } from 'expo-font';
+import { Stack } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
+import 'react-native-reanimated';
+
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
+
+export default function RootLayout() {
+    const [loaded] = useFonts({
+        Regular: require('../assets/fonts/Monteserrat-Regular.ttf'),
+        Medium: require('../assets/fonts/Monteserrat-Medium.ttf'),
+        Semibold: require('../assets/fonts/Monteserrat-Semibold.ttf'),
+    });
+
+    useEffect(() => {
+        if (loaded) {
+            SplashScreen.hideAsync();
+        }
+    }, [loaded]);
+
+    if (!loaded) {
+        return null;
+    }
+
+    return (
+        <Stack>
+        </Stack>
+    );
+}
